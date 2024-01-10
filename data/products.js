d6ad1d17379a889da544d2c1ef80ecbdebfd2613
Ines <link_ines@gmx.de>
2024-01-10 15:28:59 +0100

    problem solved gap

---
+++ data/products.js
@@ -0,0 +1,42 @@
+export const shopProducts = [
+    {
+        img: "/.assets/pro1.jpg",
+        title: "DeStoff - Kerzenhalter, 3er-Set",
+        price: "19,00",
+    },
+    {
+        img: "/.assets/pro2.jpg",
+        title: "ferm  Living - Messing-Ornament Blatt (4er-Set)",
+        price: "19,00",
+    },
+    {
+        img: "/.assets/pro3.jpg",
+        title: "HKliving - 70s - Eierbecher (4er Set)",
+        price: "19,00",
+    },
+    {
+        img: "/.assets/pro4.jpg",
+        title: "HKliving - 70s - Cappuccino Tassen 300ml, virgo (4er Set)",
+        price: "19,00",
+    },
+    {
+        img: "/.assets/pro5.jpg",
+        title: "HKliving - 70s - Salz und Pfefferstreuer (4er Set)",
+        price: "19,00",
+    },
+    {
+        img: "/.assets/pro6.jpg",
+        title: "ferm  Living - Christbaumspitze Messing",
+        price: "19,00",
+    },
+    {
+        img: "/.assets/pro7.jpg",
+        title: "HKliving - 70s - Schalendessert (4er Set)",
+        price: "19,00",
+    },
+    {
+        img: "/.assets/pro8.jpg",
+        title: "DeStoff - Kerzenhalter, 3er-Set",
+        price: "19,00",
+    },
+];
--- logic.js
+++ logic.js
@@ -1,2 +1,8 @@
-const element = document.getElementsById("h1");
-element.getElementsById("h1").innerHTML="HI"
+import { shopProducts } from "./data/products.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+    const logo =document.querySelector('header nav nav-mid h1')
+});
+for (product of shopProducts) {
+    console.log(product);
+}
